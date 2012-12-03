5f6cb505e66fb818f86437b972611e7b95ec8bd0
Mikołaj Banasik <d1sover@gmail.com>
2012-12-03 17:11:06 +0000

    Added functionality to generate tooltips for both feature and cluster markers. Cluster markers either zoom or provide list of speakers for display.

--- scripts/vendor.js
+++ scripts/vendor.js
@@ -12062,6 +12062,7 @@ mapbox.markers.layerClustered = function () {
 		clusters = [],
 		cluster_marker_cutoff,
 		calculate_clusters,
+		calculate_clusters_debounced,
 		marker_factory = base_layer.factory(),
 		cluster_factory = _.bind(mapbox.markers.simplestyle_factory, this, {
 			'properties': {
@@ -12136,7 +12137,7 @@ mapbox.markers.layerClustered = function () {
 		// No-op if not attached
 		if (!base_layer.map) { return; }
 		if (!events_bound) {
-			base_layer.map.addCallback('zoomed', _.debounce(calculate_clusters, 300));
+			base_layer.map.addCallback('zoomed', calculate_clusters_debounced);
 			events_bound = true;
 		}
 		overridden_functions.draw();
@@ -12146,7 +12147,7 @@ mapbox.markers.layerClustered = function () {
 	overridden_functions.destroy = base_layer.destroy;
 	base_layer.destroy = function () {
 		if (events_bound) {
-			base_layer.map.removeCallback('zoomed', calculate_clusters);
+			base_layer.map.removeCallback('zoomed', calculate_clusters_debounced);
 			events_bound = false;
 		}
 		overridden_functions.destroy();
@@ -12226,6 +12227,23 @@ mapbox.markers.layerClustered = function () {
 	
 	};
 	
+	(function () {
+	
+		var last_zoom = null;
+		calculate_clusters_debounced = _.debounce(function calculate_clusters_debounced () {
+		
+			var current_zoom = null;
+			if (base_layer.map) { current_zoom = base_layer.map.zoom(); }
+			
+			if (!(current_zoom && current_zoom === last_zoom)) {
+				calculate_clusters();
+				if (current_zoom) { last_zoom = current_zoom; }
+			}
+		
+		}, 300);
+	
+	}());
+	
 	base_layer.clusters = function (arr) {
 	
 		if (!arguments.length) { return clusters; }
@@ -12274,12 +12292,13 @@ mapbox.markers.layerClustered.interaction = function (clustered_layer) {
 	if (clustered_layer && clustered_layer.interaction) { return clustered_layer.interaction; }
 	
 	var interaction = {},
+		tooltips = [],
 		on = true,
 		
 		bindMarker,
+		
 		featureHandler,
 		clusterHandler,
-		
 		feature_func = function (ev) {
 			ev.preventDefault();
 			if (on && (typeof featureHandler === 'function') && clustered_layer.map) {
@@ -12291,15 +12310,18 @@ mapbox.markers.layerClustered.interaction = function (clustered_layer) {
 			if (on && (typeof clusterHandler === 'function') && clustered_layer.map) {
 				clusterHandler.apply(this, Array.prototype.slice.apply(arguments));
 			}
-		};
+		},
+		
+		featureFormatter,
+		clusterFormatter,
+		tooltipGenerator;
 	
 	interaction.enable = function () { on = true; console.log(on); };
 	interaction.disable = function () { on = false; console.log(on); };
 	
-	interaction.featureHandler = function (func) {
-		if (!arguments.length) { return featureHandler; }
-		if (typeof func === 'function') { featureHandler = func; }
-	};
+	
+	
+	
 	interaction.clusterHandler = function (func) {
 		if (!arguments.length) { return clusterHandler; }
 		if (typeof func === 'function') { clusterHandler = func; }
@@ -12318,6 +12340,10 @@ mapbox.markers.layerClustered.interaction = function (clustered_layer) {
 	};
 	
 	clusterHandler = function (ev, marker, layer) {
+	
+		var tooltip,
+			wrapper,
+			content;
 		
 		// Determine fcluster behaviour - if not at max zoom level, zoom in.
 		// Otherwise, display a tooltip containing the tooltips of all included elements.
@@ -12327,13 +12353,195 @@ mapbox.markers.layerClustered.interaction = function (clustered_layer) {
 		
 		} else {
 		
-			console.log('DISPLAY TOOLTIPS: ', arguments);
+			content = clusterFormatter(marker.data);
+			
+			if (content) {
+		
+				wrapper = document.createElement('div');
+				wrapper.style.position = 'absolute';
+				wrapper.style.pointerEvents = 'auto';
+				wrapper.style.background = 'white';
+				
+				bean.add(wrapper, 'mousedown touchstart', function stopPropagation(e) {
+					e.cancelBubble = true;
+					if (e.stopPropagation) { e.stopPropagation(); }
+					return false;
+				});
+				
+				if (typeof content === 'string') {
+					wrapper.innerHTML = content;
+				} else {
+					wrapper.appendChild(content);
+				}
+				
+				tooltip = {
+					'element': wrapper,
+					'data': {},
+					'interactive': false,
+					'location': marker.location
+				};
+				
+				tooltips.push(tooltip);
+				layer.add(tooltip);
+				layer.draw();
+			
+			}
 		
 		}
 		
 	};
-	featureHandler = function () {
-		console.log('FEATURE EVENT: ', arguments);
+	
+	interaction.featureHandler = function (func) {
+		if (!arguments.length) { return featureHandler; }
+		if (typeof func === 'function') { featureHandler = func; }
+	};
+	interaction.featureHandler(function defaultFeatureHandler (ev, marker, layer) {
+		
+		var el, tooltip, content = featureFormatter(marker.data);
+		if (content) {
+		
+			el = tooltipGenerator('feature', content);
+			tooltip = {
+				'element': el,
+				'data': {},
+				'interactive': false,
+				'location': marker.location
+			};
+			tooltips.push(tooltip);
+			layer.add(tooltip);
+			layer.draw();
+		
+		}
+		
+	});
+	
+	interaction.clusterHandler = function (func) {
+		if (!arguments.length) { return clusterHandler; }
+		if (typeof func === 'function') { clusterHandler = func; }
+	};
+	interaction.clusterHandler(function defaultClusterHandler (ev, marker, layer) {
+		
+		var el, tooltip, content;
+		
+		// Determine fcluster behaviour - if not at max zoom level, zoom in.
+		// Otherwise, display a tooltip containing the tooltips of all included elements.
+		if (layer.map.coordLimits && (layer.map.zoom() < layer.map.coordLimits[1].zoom)) {
+		
+			layer.map.centerzoom(marker.location, layer.map.zoom() + 1, true);
+		
+		} else {
+		
+			content = clusterFormatter(
+				_.pluck(marker.data.properties.items, 'data'),
+				featureFormatter
+			);
+			
+			if (content) {
+			
+				el = tooltipGenerator('cluster', content);
+				tooltip = {
+					'element': el,
+					'data': {},
+					'interactive': false,
+					'location': marker.location
+				};
+				tooltips.push(tooltip);
+				layer.add(tooltip);
+				layer.draw();
+			
+			}
+		
+		}
+		
+	});
+	
+	interaction.featureFormatter = function (func) {
+		if (!arguments.length) { return featureFormatter; }
+		if (typeof func === 'function') { featureFormatter = func; }
+	};
+	interaction.featureFormatter(function defaultFeatureFormatter (feature) {
+	
+		var html = '', el = document.createElement('div');
+		
+		if (feature.properties) {
+			if (feature.properties.name) { html += '<h1>' + feature.properties.name + '</h1>'; }
+			if (feature.properties.description) { html += '<div>' + feature.properties.description + '</div>'; }
+		}
+		el.innerHTML = html;
+		el.style.pointerEvents = 'auto';
+		
+		return el;
+	
+	});
+	
+	interaction.clusterFormatter = function (func) {
+		if (!arguments.length) { return clusterFormatter; }
+		if (typeof func === 'function') { clusterFormatter = func; }
+	};
+	interaction.clusterFormatter(function defaultClusterFormatter (features, featureFormatter) {
+	
+		var el,
+			item_list,
+			drop_zone,
+			
+			empty_func = function () {
+				while (drop_zone.firstChild) {
+					drop_zone.removeChild(drop_zone.firstChild);
+				}
+			},
+			first_item = true;
+		
+		drop_zone = document.createElement('div');
+		drop_zone.className = 'drop-zone';
+		
+		item_list = document.createElement('ul');
+		item_list.className = 'item-list';
+		
+		_.chain(features)
+			.sortBy(function (f) { return f.properties.name.toLowerCase(); })
+			.each(function itemGenerator (feature) {
+			
+				var el = document.createElement('li');
+				el.appendChild(document.createTextNode(feature.properties.name));
+				bean.add(el, 'click', function (ev) {
+					ev.preventDefault();
+					empty_func();
+					drop_zone.appendChild(featureFormatter(feature));
+				});
+				item_list.appendChild(el);
+				if (first_item) {
+					bean.fire(el, 'click');
+					first_item = false;
+				}
+			
+			});
+		
+		el = document.createElement('div');
+		el.appendChild(item_list);
+		el.appendChild(drop_zone);
+		el.style.pointerEvents = 'auto';
+		
+		return el;
+	
+	});
+	
+	tooltipGenerator = function (type, content) {
+	
+		var el = document.createElement('div');
+		el.style.position = 'absolute';
+		el.style.pointerEvents = 'none';
+		el.style.background = 'white';
+		
+		bean.add(el, 'mousedown touchstart', function stopPropagation (e) {
+			e.cancelBubble = true;
+			if (e.stopPropagation) { e.stopPropagation(); }
+			return false;
+		});
+		
+		el.appendChild(content);
+		
+		return el;
+	
 	};
 	
 	if (clustered_layer) {
--- scripts/x-utils/markersClusteredInteraction.js
+++ scripts/x-utils/markersClusteredInteraction.js
@@ -8,12 +8,13 @@ mapbox.markers.layerClustered.interaction = function (clustered_layer) {
 	if (clustered_layer && clustered_layer.interaction) { return clustered_layer.interaction; }
 	
 	var interaction = {},
+		tooltips = [],
 		on = true,
 		
 		bindMarker,
+		
 		featureHandler,
 		clusterHandler,
-		
 		feature_func = function (ev) {
 			ev.preventDefault();
 			if (on && (typeof featureHandler === 'function') && clustered_layer.map) {
@@ -25,15 +26,18 @@ mapbox.markers.layerClustered.interaction = function (clustered_layer) {
 			if (on && (typeof clusterHandler === 'function') && clustered_layer.map) {
 				clusterHandler.apply(this, Array.prototype.slice.apply(arguments));
 			}
-		};
+		},
+		
+		featureFormatter,
+		clusterFormatter,
+		tooltipGenerator;
 	
 	interaction.enable = function () { on = true; console.log(on); };
 	interaction.disable = function () { on = false; console.log(on); };
 	
-	interaction.featureHandler = function (func) {
-		if (!arguments.length) { return featureHandler; }
-		if (typeof func === 'function') { featureHandler = func; }
-	};
+	
+	
+	
 	interaction.clusterHandler = function (func) {
 		if (!arguments.length) { return clusterHandler; }
 		if (typeof func === 'function') { clusterHandler = func; }
@@ -52,6 +56,10 @@ mapbox.markers.layerClustered.interaction = function (clustered_layer) {
 	};
 	
 	clusterHandler = function (ev, marker, layer) {
+	
+		var tooltip,
+			wrapper,
+			content;
 		
 		// Determine fcluster behaviour - if not at max zoom level, zoom in.
 		// Otherwise, display a tooltip containing the tooltips of all included elements.
@@ -61,13 +69,195 @@ mapbox.markers.layerClustered.interaction = function (clustered_layer) {
 		
 		} else {
 		
-			console.log('DISPLAY TOOLTIPS: ', arguments);
+			content = clusterFormatter(marker.data);
+			
+			if (content) {
+		
+				wrapper = document.createElement('div');
+				wrapper.style.position = 'absolute';
+				wrapper.style.pointerEvents = 'auto';
+				wrapper.style.background = 'white';
+				
+				bean.add(wrapper, 'mousedown touchstart', function stopPropagation(e) {
+					e.cancelBubble = true;
+					if (e.stopPropagation) { e.stopPropagation(); }
+					return false;
+				});
+				
+				if (typeof content === 'string') {
+					wrapper.innerHTML = content;
+				} else {
+					wrapper.appendChild(content);
+				}
+				
+				tooltip = {
+					'element': wrapper,
+					'data': {},
+					'interactive': false,
+					'location': marker.location
+				};
+				
+				tooltips.push(tooltip);
+				layer.add(tooltip);
+				layer.draw();
+			
+			}
 		
 		}
 		
 	};
-	featureHandler = function () {
-		console.log('FEATURE EVENT: ', arguments);
+	
+	interaction.featureHandler = function (func) {
+		if (!arguments.length) { return featureHandler; }
+		if (typeof func === 'function') { featureHandler = func; }
+	};
+	interaction.featureHandler(function defaultFeatureHandler (ev, marker, layer) {
+		
+		var el, tooltip, content = featureFormatter(marker.data);
+		if (content) {
+		
+			el = tooltipGenerator('feature', content);
+			tooltip = {
+				'element': el,
+				'data': {},
+				'interactive': false,
+				'location': marker.location
+			};
+			tooltips.push(tooltip);
+			layer.add(tooltip);
+			layer.draw();
+		
+		}
+		
+	});
+	
+	interaction.clusterHandler = function (func) {
+		if (!arguments.length) { return clusterHandler; }
+		if (typeof func === 'function') { clusterHandler = func; }
+	};
+	interaction.clusterHandler(function defaultClusterHandler (ev, marker, layer) {
+		
+		var el, tooltip, content;
+		
+		// Determine fcluster behaviour - if not at max zoom level, zoom in.
+		// Otherwise, display a tooltip containing the tooltips of all included elements.
+		if (layer.map.coordLimits && (layer.map.zoom() < layer.map.coordLimits[1].zoom)) {
+		
+			layer.map.centerzoom(marker.location, layer.map.zoom() + 1, true);
+		
+		} else {
+		
+			content = clusterFormatter(
+				_.pluck(marker.data.properties.items, 'data'),
+				featureFormatter
+			);
+			
+			if (content) {
+			
+				el = tooltipGenerator('cluster', content);
+				tooltip = {
+					'element': el,
+					'data': {},
+					'interactive': false,
+					'location': marker.location
+				};
+				tooltips.push(tooltip);
+				layer.add(tooltip);
+				layer.draw();
+			
+			}
+		
+		}
+		
+	});
+	
+	interaction.featureFormatter = function (func) {
+		if (!arguments.length) { return featureFormatter; }
+		if (typeof func === 'function') { featureFormatter = func; }
+	};
+	interaction.featureFormatter(function defaultFeatureFormatter (feature) {
+	
+		var html = '', el = document.createElement('div');
+		
+		if (feature.properties) {
+			if (feature.properties.name) { html += '<h1>' + feature.properties.name + '</h1>'; }
+			if (feature.properties.description) { html += '<div>' + feature.properties.description + '</div>'; }
+		}
+		el.innerHTML = html;
+		el.style.pointerEvents = 'auto';
+		
+		return el;
+	
+	});
+	
+	interaction.clusterFormatter = function (func) {
+		if (!arguments.length) { return clusterFormatter; }
+		if (typeof func === 'function') { clusterFormatter = func; }
+	};
+	interaction.clusterFormatter(function defaultClusterFormatter (features, featureFormatter) {
+	
+		var el,
+			item_list,
+			drop_zone,
+			
+			empty_func = function () {
+				while (drop_zone.firstChild) {
+					drop_zone.removeChild(drop_zone.firstChild);
+				}
+			},
+			first_item = true;
+		
+		drop_zone = document.createElement('div');
+		drop_zone.className = 'drop-zone';
+		
+		item_list = document.createElement('ul');
+		item_list.className = 'item-list';
+		
+		_.chain(features)
+			.sortBy(function (f) { return f.properties.name.toLowerCase(); })
+			.each(function itemGenerator (feature) {
+			
+				var el = document.createElement('li');
+				el.appendChild(document.createTextNode(feature.properties.name));
+				bean.add(el, 'click', function (ev) {
+					ev.preventDefault();
+					empty_func();
+					drop_zone.appendChild(featureFormatter(feature));
+				});
+				item_list.appendChild(el);
+				if (first_item) {
+					bean.fire(el, 'click');
+					first_item = false;
+				}
+			
+			});
+		
+		el = document.createElement('div');
+		el.appendChild(item_list);
+		el.appendChild(drop_zone);
+		el.style.pointerEvents = 'auto';
+		
+		return el;
+	
+	});
+	
+	tooltipGenerator = function (type, content) {
+	
+		var el = document.createElement('div');
+		el.style.position = 'absolute';
+		el.style.pointerEvents = 'none';
+		el.style.background = 'white';
+		
+		bean.add(el, 'mousedown touchstart', function stopPropagation (e) {
+			e.cancelBubble = true;
+			if (e.stopPropagation) { e.stopPropagation(); }
+			return false;
+		});
+		
+		el.appendChild(content);
+		
+		return el;
+	
 	};
 	
 	if (clustered_layer) {
